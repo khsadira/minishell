0961cbf1d5fb221362da28b4b8d88fee1de0cff3
Khan Sadirac <khansadirac@macbook-pro-de-khan.home>
2018-10-09 00:15:11 +0200

    print error update

--- ft_print_error.c
+++ ft_print_error.c
@@ -14,7 +14,6 @@
 
 void	ft_no_such_file_or_dir(char *name, char *arg)
 {
-	ft_putstr_fd("minishell: ", 2);
 	ft_putstr_fd(name, 2);
 	ft_putstr_fd(arg, 2);
 	ft_putendl_fd(": no such file or directory", 2);
@@ -22,7 +21,6 @@ void	ft_no_such_file_or_dir(char *name, char *arg)
 
 void	ft_not_dir(char *name, char *arg)
 {
-	ft_putstr_fd("minishell: ", 2);
 	ft_putstr_fd(name, 2);
 	ft_putstr_fd(arg, 2);
 	ft_putendl_fd(": not a directory", 2);
@@ -30,7 +28,6 @@ void	ft_not_dir(char *name, char *arg)
 
 void	ft_permission_denied(char *name, char *arg)
 {
-	ft_putstr_fd("minishell: ", 2);
 	ft_putstr_fd(name, 2);
 	ft_putstr_fd(arg, 2);
 	ft_putendl_fd(": permission denied", 2);
